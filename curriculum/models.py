cdabcbbaa07962116d73728e83090ba07a3920af
Babajide_shadare <shadarebabaj@gmail.com>
2026-03-16 11:39:16 +0100

    Add task models for topic

--- curriculum/models.py
+++ curriculum/models.py
@@ -39,3 +39,33 @@ class Topic(TimeStampedModel):
 
     def __str__(self):
         return f"(self.category.name) - {self.title}"
+    
+class Objective(TimeStampedModel):
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="objectives",
+    )
+    description = models.TextField()
+    order = models.PositiveIntegerField(default=0)
+    class Meta:
+        ordering = ["order", "id"]
+    def __str__(self):
+        return self.description[:80]
+    
+class Task(TimeStampedModel):
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+    )
+    title = models.CharField(max_length=255)
+    instructions = models.TextField()
+    order = models.PositiveIntegerField(default=0)
+    is_mandatory = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["order", "id"]
+    
+    def __str__(self):
+        return f"{self.topic.title}" - "{self.title}"
